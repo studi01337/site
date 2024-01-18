baedd2c560dfa645bd17e760837d7b0b1710a89b
louis <louis.grellet@gmail.com>
2024-01-18 14:55:26 +0100

    og

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,39 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>French based Tech Studio</title>
+        <meta name="description" content="Elevate your digital game !" />
+
+        <meta property="og:url" content="https://studio1337.tech" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="French based Tech Studio" />
+        <meta property="og:description" content="Elevate your digital game !" />
+
+        <meta
+          property="og:image"
+          content="https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v3/studio1337.tech/French%20based%20Tech%20Studio/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2F6608be97-871e-450b-bb8c-51cc31e34981.jpg%3Ftoken%3DPfEsNF_4-Ug4YdD0tQu6EJ8P25L75cnbIF2ACreVl84%26height%3D800%26width%3D1200%26expires%3D33241585596/og.png"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="studio1337.tech" />
+        <meta property="twitter:url" content="https://studio1337.tech" />
+        <meta name="twitter:title" content="French based Tech Studio" />
+        <meta
+          name="twitter:description"
+          content="Elevate your digital game !"
+        />
+        <meta
+          name="twitter:image"
+          content="https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v3/studio1337.tech/French%20based%20Tech%20Studio/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2F6608be97-871e-450b-bb8c-51cc31e34981.jpg%3Ftoken%3DPfEsNF_4-Ug4YdD0tQu6EJ8P25L75cnbIF2ACreVl84%26height%3D800%26width%3D1200%26expires%3D33241585596/og.png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- src/layout/CavaniHead.js
+++ src/layout/CavaniHead.js
@@ -4,12 +4,10 @@ const CavaniHead = () => {
     <Head>
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="description" content="studio1337.tech" />
-      <meta name="author" content="Marketify" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1"
       />
-      <title>studio1337.tech</title>
       {/* STYLES */}
       <link
         href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
@@ -19,7 +17,29 @@ const CavaniHead = () => {
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet"
       />
-      
+
+      <title>French Tech Studio</title>
+      <meta name="description" content="Elevate your digital game !" />
+
+      <meta property="og:url" content="https://studio1337.tech" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="French Tech Studio" />
+      <meta property="og:description" content="Elevate your digital game !" />
+
+      <meta
+        property="og:image"
+        content="https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v3/studio1337.tech/French%20Tech%20Studio/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2F6608be97-871e-450b-bb8c-51cc31e34981.jpg%3Ftoken%3DrjFFKVGOb29Y-hyClKPp3D2lJ9TWI2IlNWEsm-xNuwA%26height%3D800%26width%3D1200%26expires%3D33241586025/og.png"
+      />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="studio1337.tech" />
+      <meta property="twitter:url" content="https://studio1337.tech" />
+      <meta name="twitter:title" content="French Tech Studio" />
+      <meta name="twitter:description" content="Elevate your digital game !" />
+      <meta
+        name="twitter:image"
+        content="https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v3/studio1337.tech/French%20Tech%20Studio/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fdocuments%2F6608be97-871e-450b-bb8c-51cc31e34981.jpg%3Ftoken%3DrjFFKVGOb29Y-hyClKPp3D2lJ9TWI2IlNWEsm-xNuwA%26height%3D800%26width%3D1200%26expires%3D33241586025/og.png"
+      />
     </Head>
   );
 };
